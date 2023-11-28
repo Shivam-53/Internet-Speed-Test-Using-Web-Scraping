0b020d70a0d5adc3beb2129d66d9780ec011aeaa
Shivam Tiwari <shivamn2003@gmail.com>
2023-11-28 23:33:13 +0530

    speedTest v1

---
+++ index.js
@@ -0,0 +1,45 @@
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({headless:"new"})
+  // const browser = await puppeteer.launch({headless:false})
+
+  const page = await browser.newPage();
+
+  await page.goto('https://fast.com');
+
+
+  for(let i=4000;i<=6000;i+=1000){
+
+  setTimeout(async() => {
+   await page.waitForSelector('#speed-value');
+
+    const result =  await page.evaluate(() => {
+        const soln = document.querySelector('#speed-value').innerHTML;
+        // const speedUnit=document.querySelector("#speed-units").innerHTML;
+        return soln; 
+      })
+      const speedUnit=await page.evaluate(()=>{
+        const unit=document.querySelector("#speed-units").innerHTML;
+        return unit;
+      })
+      console.log(`Speed at ${i}th ms is`,result,speedUnit);
+
+// console.log("value of i is ",i);
+
+
+if (i === 6000) {
+  await browser.close(); // Close the browser after the loop is executed
+}
+
+  }, i);
+
+
+
+  }
+
+  // await browser.close()
+
+
+})();
---
+++ speetTest.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Running Index.js
+node index.js
+echo done
+pause
