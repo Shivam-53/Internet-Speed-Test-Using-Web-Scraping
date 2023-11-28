69ee3e943eb34580a724590623b269d38fb9eb5f
Shivam Tiwari <113207307+Shivam-53@users.noreply.github.com>
2023-11-28 23:37:16 +0530

    Minor changes

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch({headless:"new"})
   // const browser = await puppeteer.launch({headless:false})
-
   const page = await browser.newPage();
-
   await page.goto('https://fast.com');
-
-
   for(let i=4000;i<=6000;i+=1000){
-
   setTimeout(async() => {
    await page.waitForSelector('#speed-value');
-
     const result =  await page.evaluate(() => {
         const soln = document.querySelector('#speed-value').innerHTML;
         // const speedUnit=document.querySelector("#speed-units").innerHTML;
@@ -25,21 +19,11 @@ import puppeteer from 'puppeteer';
         return unit;
       })
       console.log(`Speed at ${i}th ms is`,result,speedUnit);
-
 // console.log("value of i is ",i);
-
-
 if (i === 6000) {
-  await browser.close(); // Close the browser after the loop is executed
+  await browser.close(); 
 }
-
-  }, i);
-
-
-
-  }
-
+}, i);
+}
   // await browser.close()
-
-
 })();
